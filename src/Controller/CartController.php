c86d40b032fdac9a1b5d016476a588b6b3b0519a
Bastien Gauthier <bastien_gau@hotmail.fr>
2021-03-24 20:18:06 +0100

    Mise en place de la vue panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,20 +3,37 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+      $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/mon-panier", name="cart")
      */
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
+        $cartComplete = [];
+        foreach ($cart->get() as $id => $quantity) {
+          $cartComplete[] = [
+            'product' => $this->entityManager->getRepository(Product::class)->findOneBy(array('id' => $id)),
+            'quantity' => $quantity
+          ];
+        }
 
-        return $this->render('cart/index.html.twig');
+        return $this->render('cart/index.html.twig', [
+          'cart' =>$cartComplete
+        ]);
     }
 
     /**
